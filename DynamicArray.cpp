de3905d06a1399e6b20b09bdf32bba0fc200cebd
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-14 13:28:01 -0800

    10 min update

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -3,15 +3,25 @@ using namespace std;
 
 int SIZE = 5;
 
-
-
 void enterArrayData(double array[]);
 void outputArrayData(double array[]);
 double sumArray(double array[]);
 
-
 int main(){
+    // Dynamically allocated double array
+    double *ptr = nullptr;
+    ptr = new double[SIZE];
 
+    enterArrayData(ptr);
 
     return 0;
+}
+
+void enterArrayData(double array[]){
+    for (int i = 0; i < SIZE; i++){
+        cout << "Element #" << i << ": ";
+        cin >> array[i];
+    }
+    
+    cout << "Data entry complete." << endl;
 }
