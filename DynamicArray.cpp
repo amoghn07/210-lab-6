e564be9cf3bdc5b86bc689a17dcf044b1d9c850e
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-14 13:17:16 -0800

    10 min update

---
+++ DynamicArray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int SIZE = 5;
+
+
+
+void enterArrayData(double array[]);
+void outputArrayData(double array[]);
+double sumArray(double array[]);
+
+
+int main(){
+
+
+    return 0;
+}
